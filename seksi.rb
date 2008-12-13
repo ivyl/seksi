3779ea56f5b25a1ff4060a00ed20ae3b1e38645c
Arkadiusz Hiler <arek.l1@gmail.com>
2008-12-13 11:12:53 +0100

    now seksi is in single file

--- seksi.rb
+++ seksi.rb
@@ -1,4 +1,31 @@
-use_template_engine :erb
+SITE = <<'END'
+<?xml version="1.0" encoding="utf-8"?>
+<?xml-stylesheet type="text/css" href="style.css"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl">    
+<head>
+  <title>Formula!</title>
+  <script type="text/javascript">
+  var eqOld = "";
+  function update() 
+{
+     var eq = document.getElementById('formula').value;
+     eq = encodeURIComponent(eq);
+     if(eq != eqOld) { document.images[0].src = '/img/' + eq; }
+     eqOld = eq;
+}
+</script>
+</head>
+<body>
+Enter your secret forumla:</br>
+  <form action="/" method="post">
+    <textarea onkeyup="update()" "rows="10" cols="50" id="formula" name="formula">FORMULA</textarea><br/>
+    <input type="submit" value="Create" />
+  <p><img src="IMG_URL" /></p>
+</body>
+</html>
+END
 
 Merb::Config.use { |c|
   c[:framework]           = { :public => [Merb.root / "public", nil] }
@@ -16,19 +43,19 @@ end
 
 Merb.add_mime_type(:png, :to_png, %w[image/png], "Content-Type" => "image/png")
 
-class Seksi < Merb::Controller
+class Seksi < Merb::Controller    
   PRE ="\\documentclass[12pt]{article}\n\\usepackage{color}\n\\usepackage[dvips]{graphicx}\n\\pagestyle{empty}\n\\pagecolor{white}\n\\begin{document}\n{\\color{black}\n\\begin{eqnarray*}\n"
   POST = "\n\\end{eqnarray*}}\n\\end{document}"
   TEMP = '/tmp/tex2img/'
   
   def index
     if request.post?
-      @img_url = "/img/#{params[:formula].gsub('"', "''")}"
     end
-    render :template => '../../index'
+    render SITE.sub('FORMULA', params[:formula]||'').sub('IMG_URL', "/img/#{(params[:formula]||'').gsub('"', "''")}")
   end
-
+  
   def image
+    return unless params[:for]
     pwd = FileUtils.pwd
     filename = Time.now.to_f.to_s + rand.to_s
       
